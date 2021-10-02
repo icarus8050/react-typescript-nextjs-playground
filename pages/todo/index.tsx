2d46255a71d580665fdae6e0b348b7041b71ee15
icarus8050 <icarus8050@naver.com>
2021-10-03 02:31:27 +0900

    test init

---
+++ pages/todo/index.tsx
@@ -0,0 +1,11 @@
+import Hello from '../../src/todo/component/Header';
+import Body from '../../src/todo/component/Body';
+
+export default function Todo() {
+  return (
+    <>
+      <Hello />
+      <Body />
+    </>
+  );
+}
---
+++ src/todo/component/Header.tsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+  return (
+    <>
+      <div>Header Page</div>
+    </>
+  );
+}
+
+export default Hello;
